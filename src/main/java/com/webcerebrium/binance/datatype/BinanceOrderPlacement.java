78832fadfd771fd5d318bda16d583eda25eaa703
elsehow <yes@cosmopol.is>
2018-01-07 21:42:30 -0800

    skip timeInForce for MARKET orders

--- src/main/java/com/webcerebrium/binance/datatype/BinanceOrderPlacement.java
+++ src/main/java/com/webcerebrium/binance/datatype/BinanceOrderPlacement.java
@@ -51,10 +51,6 @@ public class BinanceOrderPlacement {
             throw new BinanceApiException("Order type is not set");
         }
         sb.append("&type=").append(type.toString());
-        if (timeInForce == null) {
-            throw new BinanceApiException("Order timeInForce is not set");
-        }
-        sb.append("&timeInForce=").append(timeInForce.toString());
         if (quantity == null || quantity.compareTo(BigDecimal.ZERO) <= 0) {
             throw new BinanceApiException("Order quantity should be bigger than zero");
         }
@@ -62,12 +58,15 @@ public class BinanceOrderPlacement {
 
         if (type == BinanceOrderType.MARKET) {
             // price should be skipped for a market order, we are accepting market price
-            // sb.append("&price=0");
+            // so should timeInForce
         } else {
             if (price == null || price.compareTo(BigDecimal.ZERO) <= 0) {
                 throw new BinanceApiException("Order price should be bigger than zero");
             }
             sb.append("&price=").append(price.toString());
+            if (timeInForce == null) {
+                sb.append("&timeInForce=").append(timeInForce.toString());
+            }
         }
 
         if (!Strings.isNullOrEmpty(newClientOrderId)) {
